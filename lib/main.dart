9a17f6381766e0bfd5dcd0ef6f5b1059b51a9785
Shubham Deshpande <shubhamdesh11153@gmail.com>
2020-12-15 07:07:02 -0800

    Removed Splash

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter_ebook_app/view_models/details_provider.dart';
 import 'package:flutter_ebook_app/view_models/favorites_provider.dart';
 import 'package:flutter_ebook_app/view_models/genre_provider.dart';
 import 'package:flutter_ebook_app/view_models/home_provider.dart';
-import 'package:flutter_ebook_app/views/splash/splash.dart';
+import 'package:flutter_ebook_app/views/main_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
           title: Constants.appName,
           theme: themeData(appProvider.theme),
           darkTheme: themeData(ThemeConfig.darkTheme),
-          home: Splash(),
+          home: MainScreen(),
         );
       },
     );
